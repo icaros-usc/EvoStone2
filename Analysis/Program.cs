6668e1322c046cf50f6d190cf66afe0c2f1e34b5
Yulun Zhang <yulunzha@usc.edu>
2021-04-17 15:13:06 -0700

    add analysis project

---
+++ Analysis/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Collections.Generic;
+using Nett;
+
+using SurrogateModel.Surrogate;
+
+using SabberStoneUtil.DataProcessing;
+using SabberStoneUtil.Config;
+using SabberStoneUtil.Decks;
+
+using DeckEvaluator.Config;
+using DeckEvaluator.Evaluation;
+
+using SabberStoneCore.Enums;
+using SabberStoneCore.Model;
+
+using Tensorflow;
+using NumSharp;
+using static Tensorflow.Binding;
+
+namespace Analysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = Toml.ReadFile<SabberStoneUtil.Config.Configuration>(args[0]);
+            CardReader.Init(config);
+        }
+    }
+}
