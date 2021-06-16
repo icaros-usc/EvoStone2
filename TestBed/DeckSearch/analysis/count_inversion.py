f5ef45195b1c43d281b6e97d02507002f31ad00b
Yulun Zhang <yulunzha@usc.edu>
2021-06-16 15:32:42 -0700

    move code block

--- TestBed/DeckSearch/analysis/count_inversion.py
+++ TestBed/DeckSearch/analysis/count_inversion.py
@@ -1,10 +1,15 @@
 import os
 import copy
 import toml
+import numpy as np
 import pandas as pd
 import argparse
+import tensorflow as tf
+import json
 from tqdm import tqdm
 from pprint import pprint
+from jacobian import get_latest_model_checkpoint, get_vec_encoding, build_model, calc_jacobian_matrix, card_index, card_name
+
 
 
 def get_complete_deck(elite_id, log_dir):
@@ -194,34 +199,98 @@ def count_inversion(real_order, order):
     return num_invert
 
 
-if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-l',
-    #                     '--log_dir',
-    #                     help='path to the experiment log directory',
-    #                     required=True)
-    # opt = parser.parse_args()
-    # log_dir = opt.log_dir
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l',
+                        '--log_dir',
+                        help='path to the experiment log directory',
+                        required=True)
+    opt = parser.parse_args()
+    log_dir = opt.log_dir
 
-    # # obtain order from real sim and surrogate sim
-    # orders = obtain_card_order(log_dir)
-    # pprint(orders)
+    # get all orders from remove card analysis
+    print("Getting orders from remove card analysis...")
+    rca_orders = obtain_card_order(log_dir)
 
-    real_order = [
-        'Sunwalker', 'Tirion Fordring', 'Ironfur Grizzly', 'Guardian of Kings',
-        'Consecration', 'Earthen Ring Farseer', 'Sunfury Protector',
-        'Coldlight Seer', "Sen'jin Shieldmasta", 'Stampeding Kodo',
-        'Holy Light', 'Abomination', 'Mind Control Tech', 'Fen Creeper',
-        'Lay on Hands', 'Novice Engineer', "Mogu'shan Warden",
-        'Grimscale Oracle', 'Deathwing', 'Doomsayer'
-    ]
-    order = [
-        'Tirion Fordring', 'Sunwalker', 'Consecration', 'Earthen Ring Farseer',
-        "Sen'jin Shieldmasta", 'Holy Light', 'Guardian of Kings',
-        'Stampeding Kodo', 'Mind Control Tech', 'Sunfury Protector',
-        'Grimscale Oracle', 'Coldlight Seer', 'Novice Engineer', 'Fen Creeper',
-        'Ironfur Grizzly', 'Doomsayer', 'Lay on Hands', "Mogu'shan Warden",
-        'Deathwing', 'Abomination'
-    ]
+    rm_card_analysis_dir = os.path.join(log_dir, "remove_card_analysis")
+    if not os.path.isdir(rm_card_analysis_dir):
+        raise ValueError("Remove card analysis not finished.")
+        exit(1)
 
-    count_inversion(real_order, order)
+    # read in all individuals
+    individuals = pd.read_csv(os.path.join(log_dir, "individual_log.csv"))
+
+    # get all elites to do analysis
+    real_sim_dir = os.path.join(rm_card_analysis_dir, "real_sim")
+    all_elite_decks = []
+    for elite_dir in os.listdir(real_sim_dir):
+        elite_id = int(elite_dir.split("#")[1])
+        elite_deck_str = individuals[individuals["Individual"] ==
+                                     elite_id].iloc[0]["Deck"]
+        elite_fitness = float(
+            individuals[individuals["Individual"] ==
+                        elite_id].iloc[0]["AverageHealthDifference"])
+        elite_deck = elite_deck_str.split("*")
+        all_elite_decks.append((elite_id, elite_deck, elite_fitness))
+
+    # read in model
+    model = build_model(log_dir)
+
+    # get latest model
+    model_checkpoint = get_latest_model_checkpoint(log_dir)
+
+    num_inversions = {}
+    print("Getting orders from jacobian analysis and counting inversions...")
+    for elite_id, elite_deck, elite_fitness in tqdm(all_elite_decks):
+        with tf.compat.v1.Session() as sess:
+            # load model params
+            saver = tf.compat.v1.train.Saver()
+            saver.restore(sess, model_checkpoint)
+
+            # encode deck
+            x = get_vec_encoding(elite_deck)
+            # print(x)
+
+            jacobian_matrix = calc_jacobian_matrix(model, x, sess)
+
+            # print("Jacobian matrix of Fitness value:")
+            # print(jacobian_matrix[0, 0])
+            # print(jacobian_matrix.shape)
+
+            # get the order of cards
+            fitness_jacobian = copy.deepcopy(jacobian_matrix[0, 0])
+            top_card_index = fitness_jacobian.argsort()
+
+            card_names_by_pw = []
+            num_cards = []
+            card_values = []
+            for idx in np.flip(top_card_index):
+                if x[0, idx] != 0:
+                    card_names_by_pw.append(card_name[idx])
+                    num_cards.append(x[0, idx])
+                    card_values.append(fitness_jacobian[idx])
+            # print("'Value' of card (Large to small):")
+            # print(card_values)
+            # print("Cards:")
+            # print(card_names_by_pw)
+            # print("Number of cards:")
+            # print(num_cards)
+
+            # get the order from remove card analysis
+            real_order = [
+                card for card, _ in rca_orders[elite_id]["real_order"]
+            ]
+
+            # calculate num inversions
+            num_inversions[elite_id] = {
+                "inversions": count_inversion(real_order, card_names_by_pw),
+                "fitness": elite_fitness,
+            }
+
+        # reset model
+        tf.compat.v1.reset_default_graph()
+        model = build_model(log_dir)
+
+    with open(os.path.join(log_dir, "inversions.json"), "w") as f:
+        json.dump(num_inversions, f)
+    # pprint(num_inversions)
--- TestBed/DeckSearch/analysis/jacobian.py
+++ TestBed/DeckSearch/analysis/jacobian.py
@@ -9,15 +9,11 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 from surrogate_model import FCNN, DeepSet, LinearModel
-from count_inversion import obtain_card_order, count_inversion
 from tqdm import tqdm
 from pprint import pprint
+from utils import read_in_card_index
 
-# read in card index
-with open('analysis/paladin_card_index.json') as f:
-    card_index = json.load(f)
-
-card_name = {idx: name for name, idx in card_index.items()}
+card_index, card_name = read_in_card_index()
 
 
 def get_latest_model_checkpoint(log_dir):
@@ -64,7 +60,7 @@ def build_model(log_dir):
     return model
 
 
-def calc_jacobian_matrix(model, x):
+def calc_jacobian_matrix(model, x, sess):
     """
     Calculates the jacobian matrix of the model with input x.
     """
@@ -79,100 +75,3 @@ def calc_jacobian_matrix(model, x):
 
     jacobian_matrix = np.concatenate(jacobian_matrix, axis=1)
     return jacobian_matrix
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-l',
-                        '--log_dir',
-                        help='path to the experiment log directory',
-                        required=True)
-    opt = parser.parse_args()
-    log_dir = opt.log_dir
-
-    # get all orders from remove card analysis
-    print("Getting orders from remove card analysis...")
-    rca_orders = obtain_card_order(log_dir)
-
-    rm_card_analysis_dir = os.path.join(log_dir, "remove_card_analysis")
-    if not os.path.isdir(rm_card_analysis_dir):
-        raise ValueError("Remove card analysis not finished.")
-        exit(1)
-
-    # read in all individuals
-    individuals = pd.read_csv(os.path.join(log_dir, "individual_log.csv"))
-
-    # get all elites to do analysis
-    real_sim_dir = os.path.join(rm_card_analysis_dir, "real_sim")
-    all_elite_decks = []
-    for elite_dir in os.listdir(real_sim_dir):
-        elite_id = int(elite_dir.split("#")[1])
-        elite_deck_str = individuals[individuals["Individual"] ==
-                                     elite_id].iloc[0]["Deck"]
-        elite_fitness = float(
-            individuals[individuals["Individual"] ==
-                        elite_id].iloc[0]["AverageHealthDifference"])
-        elite_deck = elite_deck_str.split("*")
-        all_elite_decks.append((elite_id, elite_deck, elite_fitness))
-
-    # read in model
-    model = build_model(log_dir)
-
-    # get latest model
-    model_checkpoint = get_latest_model_checkpoint(log_dir)
-
-    num_inversions = {}
-    print("Getting orders from jacobian analysis and counting inversions...")
-    for elite_id, elite_deck, elite_fitness in tqdm(all_elite_decks):
-        with tf.compat.v1.Session() as sess:
-            # load model params
-            saver = tf.compat.v1.train.Saver()
-            saver.restore(sess, model_checkpoint)
-
-            # encode deck
-            x = get_vec_encoding(elite_deck)
-            # print(x)
-
-            jacobian_matrix = calc_jacobian_matrix(model, x)
-
-            # print("Jacobian matrix of Fitness value:")
-            # print(jacobian_matrix[0, 0])
-            # print(jacobian_matrix.shape)
-
-            # get the order of cards
-            fitness_jacobian = copy.deepcopy(jacobian_matrix[0, 0])
-            top_card_index = fitness_jacobian.argsort()
-
-            card_names_by_pw = []
-            num_cards = []
-            card_values = []
-            for idx in np.flip(top_card_index):
-                if x[0, idx] != 0:
-                    card_names_by_pw.append(card_name[idx])
-                    num_cards.append(x[0, idx])
-                    card_values.append(fitness_jacobian[idx])
-            # print("'Value' of card (Large to small):")
-            # print(card_values)
-            # print("Cards:")
-            # print(card_names_by_pw)
-            # print("Number of cards:")
-            # print(num_cards)
-
-            # get the order from remove card analysis
-            real_order = [
-                card for card, _ in rca_orders[elite_id]["real_order"]
-            ]
-
-            # calculate num inversions
-            num_inversions[elite_id] = {
-                "inversions": count_inversion(real_order, card_names_by_pw),
-                "fitness": elite_fitness,
-            }
-
-        # reset model
-        tf.compat.v1.reset_default_graph()
-        model = build_model(log_dir)
-
-    with open(os.path.join(log_dir, "inversions.json"), "w") as f:
-        json.dump(num_inversions, f)
-    # pprint(num_inversions)
