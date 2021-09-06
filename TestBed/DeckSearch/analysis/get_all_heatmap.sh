f824b3051f3f9ecd8b220b4ac28c3bc4a4044fb5
Yulun Zhang <yulunzha@usc.edu>
2021-09-06 16:19:38 -0700

    script to get the heatmap in one shot

--- TestBed/DeckSearch/analysis/get_all_heatmap.sh
+++ TestBed/DeckSearch/analysis/get_all_heatmap.sh
@@ -14,12 +14,12 @@ declare -a all_more_exp=(
 )
 
 
-# for log_dir in "${all_base_exp[@]}"
-# do
-#     echo "Log Dir: ${log_dir}"
-#     python analysis/gen_metrics.py -l "${log_dir}" -s $step_length
-#     cp $log_dir/metrics/elites_archive/heatmap/*.pdf ~/default_targets/
-# done
+for log_dir in "${all_base_exp[@]}"
+do
+    echo "Log Dir: ${log_dir}"
+    python analysis/gen_metrics.py -l "${log_dir}" -s $step_length
+    cp $log_dir/metrics/elites_archive/heatmap/*.pdf ~/default_targets/
+done
 
 
 for log_dir in "${all_more_exp[@]}"
